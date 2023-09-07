290c6ad0dd45407620d2f4865cc85992c45fd7f3
copyrat90 <copyrat90@gmail.com>
2023-09-07 21:07:09 +0900

    Add support for C array export

--- 116_vgm2gba_vblank/src/tool/vgm2gba/vgm2gba.py
+++ 116_vgm2gba_vblank/src/tool/vgm2gba/vgm2gba.py
@@ -102,80 +102,114 @@ class VgmFile:
 
             raise VgmFormatError(f"Commands. offset 0x{p:02x} = 0x{data[p]:02x}")
 
-    def write_output(self, output_path):
+    def write_binary(self, output_path):
+        with open(output_path, "wb") as file:
+            file.write(self.__get_converted_data())
+
+    def write_c_array(self, output_path, c_array_identifier):
+        converted = self.__get_converted_data()
+
+        with open(output_path, "w") as file:
+            file.write(f"const unsigned char {c_array_identifier}[] = {{\n")
+            for num in converted:
+                file.write(f"0x{num:02x},")
+            file.write("};\n")
+
+            file.write(
+                f"const unsigned int {c_array_identifier}_len = {len(converted)};\n"
+            )
+
+    def __get_converted_data(self):
         data = self.data
 
-        with open(output_path, "wb") as file:
-            is_loop = False
-            loop_bin = 0
-            loop_vgm = (
-                data[0x1C] | (data[0x1D] << 8) | (data[0x1E] << 16) | (data[0x1F] << 24)
-            )
-            loop_vgm += 0x1C
+        converted = bytearray()
 
-            print(f"VgmLoopOffset: 0x{loop_vgm:02x}")
+        is_loop = False
+        loop_bin = 0
+        loop_vgm = (
+            data[0x1C] | (data[0x1D] << 8) | (data[0x1E] << 16) | (data[0x1F] << 24)
+        )
+        loop_vgm += 0x1C
 
-            p = 0xC0
-            fputc_cnt = 0
+        print(f"VgmLoopOffset: 0x{loop_vgm:02x}")
 
-            while data[p] != 0x66:  # end of mark
-                # check loop offset
-                if p == loop_vgm:
-                    print(f"BinLoopOffset: 0x{fputc_cnt:02x}")
+        p = 0xC0
+        fputc_cnt = 0
 
-                    loop_bin = fputc_cnt
-                    is_loop = True
+        while data[p] != 0x66:  # end of mark
+            # check loop offset
+            if p == loop_vgm:
+                print(f"BinLoopOffset: 0x{fputc_cnt:02x}")
 
-                # wait: 0x61 nn nn
-                if data[p] == 0x61:
-                    # GBA side use vblank
-                    file.write(data[p].to_bytes())
-                    p += 1
-                    fputc_cnt += 1
-                    # ignore param
-                    p += 2
-                    continue
+                loop_bin = fputc_cnt
+                is_loop = True
 
-                # write reg: 0xb3 aa dd
-                if data[p] == 0xB3:
-                    d2 = data[p + 1]
+            # wait: 0x61 nn nn
+            if data[p] == 0x61:
+                # GBA side use vblank
+                converted += data[p].to_bytes()
+                p += 1
+                fputc_cnt += 1
+                # ignore param
+                p += 2
+                continue
 
-                    file.write(data[p : p + 3])
-                    p += 3
+            # write reg: 0xb3 aa dd
+            if data[p] == 0xB3:
+                d2 = data[p + 1]
+
+                converted += data[p : p + 3]
+                p += 3
+                fputc_cnt += 3
+
+                # GBA patch
+
+                # wave adr?
+                if 0x90 <= d2 <= 0x9F:
+                    # add REG_SOUND3CNT_L = 0x40;
+                    converted += b"\xb3\x70\x40"
                     fputc_cnt += 3
 
-                    # GBA patch
+                continue
 
-                    # wave adr?
-                    if 0x90 <= d2 <= 0x9F:
-                        # add REG_SOUND3CNT_L = 0x40;
-                        file.write(b"\xb3\x70\x40")
-                        fputc_cnt += 3
+        if not is_loop:
+            print("Warning: loop offset not found")
 
-                    continue
+        # write end of mark
+        converted += data[p].to_bytes()
+        p += 1
 
-            if not is_loop:
-                print("Warning: loop offset not found")
+        # write loop offset
+        converted += loop_bin.to_bytes(4, byteorder="little")
 
-            # write end of mark
-            file.write(data[p].to_bytes())
-            p += 1
+        # zero padding
+        pad = len(converted) & 0xF
+        if pad != 0:
+            converted += b"\x00" * (0x10 - pad)
 
-            # write loop offset
-            file.write(loop_bin.to_bytes(4, byteorder="little"))
-
-            # zero padding
-            pad = file.tell() & 0xF
-            if pad != 0:
-                file.write(b"\x00" * (0x10 - pad))
+        return converted
 
 
-def convert_file(vgm_path, output_path):
+def convert_file_binary(vgm_path, output_path):
     with open(vgm_path, "rb") as file:
         file_byte_array = bytearray(file.read())
 
     vgm = VgmFile(file_byte_array)
-    vgm.write_output(output_path)
+    vgm.write_binary(output_path)
+
+
+def convert_file_c_array(vgm_path, output_path, c_array_identifier=None):
+    if not c_array_identifier:
+        import os
+
+        output_filename = os.path.splitext(os.path.split(output_path)[-1])[0]
+        c_array_identifier = output_filename
+
+    with open(vgm_path, "rb") as file:
+        file_byte_array = bytearray(file.read())
+
+    vgm = VgmFile(file_byte_array)
+    vgm.write_c_array(output_path, c_array_identifier)
 
 
 if __name__ == "__main__":
@@ -193,22 +227,41 @@ if __name__ == "__main__":
         description="convert hUGETracker VGM files into VGM Player binary format."
     )
     parser.add_argument(
-        "--input", default=None, required=True, help="input hUGETracker VGM file"
+        "--input", default=None, required=True, help="input hUGETracker VGM file path"
     )
     parser.add_argument(
-        "--output", default=None, required=False, help="output VGM Player binary file"
+        "--output", default=None, required=False, help="output file path"
+    )
+    parser.add_argument(
+        "--c-array",
+        nargs="?",
+        default=None,
+        const=True,
+        required=False,
+        help="output as C array, instead of binary. Optionally, you can pass a desired identifier for the array.",
     )
 
     args = parser.parse_args()
 
-    # Use `input_filename.bin` as output file path
     if not args.output:
         import os
 
-        args.output = os.path.splitext(args.input)[0] + ".bin"
+        # Use `input_filename.bin` as output file path
+        ext_str = ".c" if args.c_array else ".bin"
+        args.output = os.path.splitext(args.input)[0] + ext_str
+
+    # `--c-array` without argument
+    if type(args.c_array) == bool:
+        import os
+
+        # Use `output_filename` as C array identifier
+        args.c_array = os.path.splitext(os.path.split(args.output)[-1])[0]
 
     try:
-        convert_file(args.input, args.output)
+        if args.c_array:
+            convert_file_c_array(args.input, args.output, args.c_array)
+        else:
+            convert_file_binary(args.input, args.output)
     except VgmFormatError as e:
         print("ERROR: Invalid VGM file: " + str(e))
         sys.exit(1)
